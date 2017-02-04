73b6fd94105b4f5874c2da4dca32eebc0f308902
Daniel Seidler <daniel.seidler@gmail.com>
2017-02-04 09:57:07 +0100

    Initial commit

---
+++ lib/checksum.ex
@@ -0,0 +1,18 @@
+defmodule Checksum do
+  @moduledoc """
+  Documentation for Checksum.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Checksum.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
